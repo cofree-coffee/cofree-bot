59053fa22155fcf9b3d318cdb64bcaf2aff5d0d0
solomon <ssbothwell@gmail.com>
2021-12-28 15:29:20 -0800

    Fixes flake

--- flake.nix
+++ flake.nix
@@ -16,29 +16,37 @@
   };
 
   outputs = { self, nixpkgs, easy-hls, flake-utils}:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-          hls = pkgs.callPackage easy-hls {
-            ghcVersions = [ "8.10.7" ];
-          };
-      in rec {
-        devShell = pkgs.haskellPackages.shellFor {
-          packages = _: [];
-          buildInputs = [
-            pkgs.haskellPackages.cabal-install
-            pkgs.haskellPackages.ghc
-            pkgs.zlib
-            pkgs.zlib.dev
-            pkgs.pkg-config
-            hls
-          ];
-          shellHook = ''
-            export LD_LIBRARY_PATH=${pkgs.zlib}/lib
-            export DYLD_LIBRARY_PATH=${pkgs.zlib}/lib
-            export PATH=$PATH:$HOME/.local/bin
-          '';
+    let overlay = final: prev: {
+          haskellPackages = prev.haskellPackages.override (old: {
+            overrides = prev.lib.composeExtensions (old.overrides or (_: _: {}))
+              (hfinal: hprev: {
+                cofree-bot = hfinal.callCabal2nix "cofree-bot" ./. {};
+                matrix-client = hfinal.callCabal2nixWithOptions "matrix-client" (final.fetchFromGitHub {
+                  owner = "cofree-coffee";
+                  repo = "matrix-client-haskell";
+                  rev = "3aad90a0c836dfb1235e05ab6311d340f7a48dcf";
+                  sha256 = "sha256-96AQqAHl8PLRdlhL/hoxcGpfXGMZxsB6Pmgzjl1uDt4=";
+                }) "--subpath matrix-client" { };
+              });
+          });
         };
-        defaultPackage =
-          pkgs.haskellPackages.callCabal2nix "cofree-bot" ./. {};
-      });
+        overlays = [ overlay ];
+    in flake-utils.lib.eachDefaultSystem (system:
+        let pkgs = import nixpkgs { inherit system overlays; };
+            hls = pkgs.callPackage easy-hls {
+              ghcVersions = [ "8.10.7" ];
+            };
+        in rec {
+          devShell = pkgs.haskellPackages.shellFor {
+            packages = _: [];
+            buildInputs = [
+              pkgs.haskellPackages.cabal-install
+              pkgs.haskellPackages.ghc
+              pkgs.zlib
+              hls
+            ];
+          };
+          defaultPackage =
+            pkgs.haskellPackages.cofree-bot;
+        }) // { inherit overlay overlays; };
 }
