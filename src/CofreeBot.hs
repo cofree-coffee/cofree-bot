f7837ab786277338563c99e07469a040ad7956ed
solomon <ssbothwell@gmail.com>
2022-01-02 00:39:47 -0800

    Removes Session module

--- src/CofreeBot.hs
+++ src/CofreeBot.hs
@@ -4,7 +4,6 @@ module CofreeBot ( module Bot
                  , module Hello
                  , module Matrix
                  , module Simple
-                 , module Session
                  ) where
 
 import CofreeBot.Bot as Bot
@@ -13,4 +12,3 @@ import CofreeBot.Bot.Calculator as Calculator
 import CofreeBot.Bot.Hello as Hello
 import CofreeBot.Bot.Matrix as Matrix
 import CofreeBot.Bot.Simple as Simple
-import CofreeBot.Session as Session
--- src/CofreeBot/Session.hs
+++
@@ -1,5 +0,0 @@
-module CofreeBot.Session where
-
-import Data.Text qualified as T
-
-newtype MatrixServer = MatrixServer { getMatrixServer :: T.Text }
