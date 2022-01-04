c34c42ec3c4e8f60ba34d40cd7189d0ac192b98a
solomon <ssbothwell@gmail.com>
2022-01-03 18:45:26 -0800

    Removes unused function

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,6 @@ import Data.Text qualified as T
 import Network.Matrix.Client
 import Options.Applicative qualified as Opt
 import OptionsParser
-import System.Environment ( getEnv, lookupEnv )
 import System.Environment.XDG.BaseDir ( getUserCacheDir )
 {-
 *This is a very scrappy rough draft*
@@ -21,11 +20,6 @@ TODO:
 - [ ] Add fixed point of Bot
 - [ ] Bot should auto join DMs
 -}
-
-findXdgCache :: IO String
-findXdgCache = do
-  home <- getEnv "HOME"
-  fmap (maybe (home <> "/.cache/cofree-bot") (<> "/cofree-bot")) $ lookupEnv "XDG_CACHE_HOME"
 
 main :: IO ()
 main = do
