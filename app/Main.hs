631b7db38347265287781ff70bf857b8c84fc005
solomon <ssbothwell@gmail.com>
2022-01-11 14:17:11 -0800

    GHCI Bot V2

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,37 @@
+{-# LANGUAGE NumDecimals #-}
 module Main where
 
 import CofreeBot
 import CofreeBot.Bot.Calculator.Language
+import Control.Monad
 import Data.Profunctor
 import Data.Text qualified as T
+import GHC.Conc (threadDelay)
 import Network.Matrix.Client
 import Options.Applicative qualified as Opt
 import OptionsParser
 import System.Environment.XDG.BaseDir ( getUserCacheDir )
+import System.Process.Typed
+
+--main :: IO ()
+--main = withProcessWait_ ghciConfig $ \process -> do
+--  runSimpleBot (simplifySessionBot (T.intercalate "\n" . printCalcOutput) programP $ sessionize mempty $ calculatorBot) mempty
+--  runSimpleBot (ghciBot process) mempty
+
+--  let ghciBot' = ghciBot process
+--      calcBot = simplifySessionBot (T.intercalate "\n" . printCalcOutput) programP $ sessionize mempty $ calculatorBot
+--  runSimpleBot (rmap (\(x :& y) -> x <> y ) $ ghciBot' /\ calcBot) (mempty)
 
 main :: IO ()
-main = do
-  --runSimpleBot (simplifySessionBot (T.intercalate "\n" . printCalcOutput) programP $ sessionize mempty $ calculatorBot) mempty
-  --runSimpleBot (ghciBot) mempty
-
-  --let ghciBot' = simplifySessionBot (T.intercalate "\n") ghciInputParser $ sessionize mempty $ ghciBot
-  --    calcBot = simplifySessionBot (T.intercalate "\n" . printCalcOutput) programP $ sessionize mempty $ calculatorBot
-  --runSimpleBot (rmap (\(x :& y) -> x <> y ) $ ghciBot' /\ calcBot) (mempty)
-
+main = withProcessWait_ ghciConfig $ \process -> do
+  void $ threadDelay 5e5
+  void $ hGetOutput (getStdout process)
   command <- Opt.execParser parserInfo
   xdgCache <- getUserCacheDir "cofree-bot"
   let calcBot = liftSimpleBot $ simplifySessionBot (T.intercalate "\n" . printCalcOutput) programP $ sessionize mempty $ calculatorBot
       helloBot = helloMatrixBot
       coinFlipBot' = liftSimpleBot $ simplifyCoinFlipBot coinFlipBot
-      ghciBot' = liftSimpleBot $ ghciBot
+      ghciBot' = liftSimpleBot $ ghciBot process
       bot = rmap (\(x :& y :& z :& q) -> x <> y <> z <> q) $ calcBot /\ helloBot /\ coinFlipBot' /\ ghciBot'
   case command of
     LoginCmd cred -> do
--- src/CofreeBot/Bot/GHCI.hs
+++ src/CofreeBot/Bot/GHCI.hs
@@ -1,26 +1,40 @@
+{-# LANGUAGE NumDecimals #-}
 module CofreeBot.Bot.GHCI where
 
-
 import CofreeBot.Bot
-import CofreeBot.Bot.GHCI.Interpreter
 import Control.Monad
+import Control.Monad.Loops (whileM)
 import Data.Attoparsec.Text as A
 import Data.Text qualified as T
-import Data.Bifunctor
-import Control.Lens
+import System.Process.Typed
+import System.IO
+import GHC.Conc (threadDelay)
+import Data.Profunctor
 
-ghciBot :: Bot IO () T.Text [T.Text]
-ghciBot = Bot $ \i s -> do
-  case parseOnly ghciInputParser i of
-    Left _ -> pure $ BotAction [] s
-    Right input -> do
-      result <- fmap (bimap T.pack (fmap T.pack)) <$> runGhci input
-      let result' = result & fmap (\case
-           (i', Nothing) -> "```\n> " <> i' <> "\n````"
-           (i', Just o) -> "```\n> " <> i' <> "\n" <> o <> "\n```")
-      pure $ BotAction result' s
+type GhciBot = Bot IO () T.Text [T.Text]
 
+hGetOutput :: Handle -> IO String
+hGetOutput handle =
+  whileM (hReady handle) (hGetChar handle) 
+    
+ghciBot' :: Process Handle Handle () -> GhciBot
+ghciBot' p = mapMaybeBot (either (const Nothing) Just . parseOnly ghciInputParser) $ Bot $ \i s -> do
+  hPutStrLn (getStdin p) $ T.unpack i
+  hFlush (getStdin p)
+  void $ threadDelay 5e5
+  o <- hGetOutput (getStdout p)
+  pure $ BotAction (pure $ T.pack o) s
+
+ghciBot :: Process Handle Handle () -> GhciBot
+ghciBot p = dimap (\i -> if i == "ghci: :q" then Left i else Right i) (either id id) $ pureStatelessBot (const $ ["I'm Sorry Dave"]) \/ ghciBot' p
+
+ghciConfig :: ProcessConfig Handle Handle ()
+ghciConfig = setStdin createPipe
+          $ setStdout createPipe
+          $ shell "docker run -i haskell 2>&1"
+  
 ghciInputParser :: Parser T.Text
 ghciInputParser = do
-  void $ "$ "
+  void $ "ghci: "
   T.pack <$> many1 anyChar 
+ 
