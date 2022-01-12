3d9b0d9d80d7623f7aa8526a8fc83b0be5dab8c7
Jonathan Lorimer <jonathan_lorimer@mac.com>
2022-01-11 19:31:03 -0500

    test failing ci

--- docker.nix
+++ docker.nix
@@ -1,18 +1,19 @@
-{ pkgs, overlay }:
+{ pkgs, cofree-bot }:
 
 pkgs.dockerTools.buildLayeredImage {
-    name = "ghcr.io/cofree-coffee/cofree-bot";
-    created = "now";
-    tag = "latest";
-    contents = [ 
-      pkgs.bash
-      pkgs.docker
-      pkgs.cacert
+  name = "ghcr.io/cofree-coffee/cofree-bot";
+  created = "now";
+  tag = "latest";
+  contents = [
+    pkgs.bash
+    pkgs.cacert
+  ];
+  config = {
+    Entrypoint = "${pkgs.bash}/bin/bash";
+    Cmd = [
+      "-c"
+      "${pkgs.haskell.lib.justStaticExecutables cofree-bot}/bin/cofree-bot run --auth_token $AUTH_TOKEN --homeserver https://matrix.cofree.coffee"
     ];
-    config = {
-      Entrypoint = "${pkgs.bash}/bin/bash";
-      Cmd = [ 
-        "-c" "${pkgs.haskell.lib.justStaticExecutables pkgs.haskellPackages.cofree-bot}/bin/cofree-bot run --auth_token $AUTH_TOKEN --homeserver https://matrix.cofree.coffee"
-      ];
-    };
-  }
+  };
+}
+
--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 {
+
   description = "Cofree.Coffee Matrix Bot";
 
   inputs = {
@@ -9,32 +10,83 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    easy-hls = {
-      url = github:ssbothwell/easy-hls-nix;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, easy-hls, flake-utils}:
-    let overlay = import ./overlay.nix { compiler = "ghc8107"; };
-        overlays = [ overlay ];
-    in flake-utils.lib.eachDefaultSystem (system:
-        let pkgs = import nixpkgs { inherit system overlays; };
-            hls = pkgs.callPackage easy-hls {
-              ghcVersions = [ "8.10.7" ];
-            };
-        in rec {
-          devShell = pkgs.haskellPackages.shellFor {
-            packages = _: [];
-            buildInputs = [
-              pkgs.haskellPackages.cabal-install
-              pkgs.haskellPackages.ghc
-              pkgs.zlib
-              hls
-            ];
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
+    let
+      ghcVersion = "8107";
+      compiler = "ghc${ghcVersion}";
+      # default systems compatible with pre-commit-hooks.nix
+      # https://github.com/cachix/pre-commit-hooks.nix/pull/122
+      defaultSystems = [
+        "aarch64-linux"
+        # "aarch64-darwin"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    flake-utils.lib.eachSystem defaultSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        # need to do the evalPkgs trick so that IFD works with `nix flake check`
+        # https://github.com/NixOS/nix/issues/4265
+        evalPkgs = import nixpkgs { system = "x86_64-linux"; };
+        hsPkgs = evalPkgs.haskell.packages.${compiler}.override {
+          overrides = hfinal: hprev: {
+            cofree-bot = hfinal.callCabal2nix "cofree-bot" ./. { };
+
+            # command to reproduce:
+            # cabal2nix https://github.com/softwarefactory-project/matrix-client-haskell --subpath matrix-client --revision f8610d8956bd146105292bb75821ca078d01b5ff > .nix/deps/matrix-client.nix
+            matrix-client = hfinal.callPackage ./.nix/deps/matrix-client.nix { };
           };
-          defaultPackage =
-            pkgs.haskellPackages.cofree-bot;
-          packages.docker = import ./docker.nix { inherit pkgs overlay; };
-        } // { inherit overlay overlays; });
+        };
+        brittany-config = pkgs.writeTextFile "brittany-config" (builtins.readFile ./brittany.yaml);
+      in
+      rec {
+
+        # Note: cannot reference anything that depends on `evalPkgs` like `hsPkgs`
+        # otherwise non-x86_64-linux users will not be able to build the dev env
+        devShell = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          buildInputs = with pkgs; [
+            haskellPackages.brittany
+            (haskell-language-server.override { supportedGhcVersions = [ ghcVersion ]; })
+            cabal-install
+            ghc
+            ghcid
+            cabal2nix
+            zlib
+          ];
+        };
+
+        packages = flake-utils.lib.flattenTree {
+          docker = import ./docker.nix {
+            inherit pkgs;
+            cofree-bot = hsPkgs.cofree-bot;
+          };
+          cofree-bot = hsPkgs.cofree-bot;
+        };
+
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              # brittany = {
+              #   name = "brittany";
+              #   entry = "${hsPkgs.brittany}/bin/brittany --write-mode=inplace --config-file=${brittany-config}";
+              #   files = "\\.l?hs$";
+              #   language = "system";
+              #   pass_filenames = false;
+              # };
+              cabal-fmt.enable = true;
+            };
+          };
+        };
+
+        defaultPackage = packages.cofree-bot;
+      });
 }
