842297694c7f65ed7a2047c78182511429c9e4dd
solomon <ssbothwell@gmail.com>
2022-01-14 12:13:16 -0800

    Removes ghci bot from main.

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,12 @@ module Main where
 
 import           CofreeBot
 import           CofreeBot.Bot.Behaviors.Calculator.Language
-import           Control.Monad
 import           Data.Profunctor
 import qualified Data.Text                     as T
-import           GHC.Conc                       ( threadDelay )
 import           Network.Matrix.Client
 import qualified Options.Applicative           as Opt
 import           OptionsParser
 import           System.Environment.XDG.BaseDir ( getUserCacheDir )
-import           System.Process.Typed
 
 --main :: IO ()
 --main = withProcessWait_ ghciConfig $ \process -> do
@@ -23,9 +20,7 @@ import           System.Process.Typed
 --  runSimpleBot (rmap (\(x :& y) -> x <> y ) $ ghciBot' /\ calcBot) (mempty)
 
 main :: IO ()
-main = withProcessWait_ ghciConfig $ \process -> do
-  void $ threadDelay 1e6
-  void $ hGetOutput (getStdout process)
+main = do
   command  <- Opt.execParser parserInfo
   xdgCache <- getUserCacheDir "cofree-bot"
   let calcBot =
@@ -35,13 +30,11 @@ main = withProcessWait_ ghciConfig $ \process -> do
           $ calculatorBot
       helloBot     = helloMatrixBot
       coinFlipBot' = liftSimpleBot $ simplifyCoinFlipBot coinFlipBot
-      ghciBot'     = liftSimpleBot $ ghciBot process
       bot =
-        rmap (\(x :& y :& z :& q) -> x <> y <> z <> q)
+        rmap (\(x :& y :& z) -> x <> y <> z)
           $  calcBot
           /\ helloBot
           /\ coinFlipBot'
-          /\ ghciBot'
   case command of
     LoginCmd cred -> do
       session <- login cred
