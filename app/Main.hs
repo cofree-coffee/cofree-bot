52f9b747a0e070247e5a7e3bfbc7e018f436d6e9
Asad Saeeduddin <masaeedu@gmail.com>
2022-01-14 19:14:29 -0500

    Revert "Removes ghci bot from main."
    This reverts commit 842297694c7f65ed7a2047c78182511429c9e4dd.

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,15 @@ module Main where
 
 import           CofreeBot
 import           CofreeBot.Bot.Behaviors.Calculator.Language
+import           Control.Monad
 import           Data.Profunctor
 import qualified Data.Text                     as T
+import           GHC.Conc                       ( threadDelay )
 import           Network.Matrix.Client
 import qualified Options.Applicative           as Opt
 import           OptionsParser
 import           System.Environment.XDG.BaseDir ( getUserCacheDir )
+import           System.Process.Typed
 
 --main :: IO ()
 --main = withProcessWait_ ghciConfig $ \process -> do
@@ -20,7 +23,9 @@ import           System.Environment.XDG.BaseDir ( getUserCacheDir )
 --  runSimpleBot (rmap (\(x :& y) -> x <> y ) $ ghciBot' /\ calcBot) (mempty)
 
 main :: IO ()
-main = do
+main = withProcessWait_ ghciConfig $ \process -> do
+  void $ threadDelay 1e6
+  void $ hGetOutput (getStdout process)
   command  <- Opt.execParser parserInfo
   xdgCache <- getUserCacheDir "cofree-bot"
   let calcBot =
@@ -30,11 +35,13 @@ main = do
           $ calculatorBot
       helloBot     = helloMatrixBot
       coinFlipBot' = liftSimpleBot $ simplifyCoinFlipBot coinFlipBot
+      ghciBot'     = liftSimpleBot $ ghciBot process
       bot =
-        rmap (\(x :& y :& z) -> x <> y <> z)
+        rmap (\(x :& y :& z :& q) -> x <> y <> z <> q)
           $  calcBot
           /\ helloBot
           /\ coinFlipBot'
+          /\ ghciBot'
   case command of
     LoginCmd cred -> do
       session <- login cred
