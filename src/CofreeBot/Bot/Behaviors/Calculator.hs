be10d94909ba3c32502643fa219ba3ede308d99b
Conjunctive <conjunctive@protonmail.com>
2022-01-19 16:23:56 -0800

    Port Calculator Behavior to ListT Bot

--- src/CofreeBot/Bot/Behaviors/Calculator.hs
+++ src/CofreeBot/Bot/Behaviors/Calculator.hs
@@ -3,34 +3,38 @@ module CofreeBot.Bot.Behaviors.Calculator where
 import           CofreeBot.Bot
 import           CofreeBot.Bot.Behaviors.Calculator.Language
 import           CofreeBot.Utils
-import           Data.Functor
+import           CofreeBot.Utils.ListT
+import           Data.Bitraversable
+import           Data.Function ((&))
+import           Data.Functor ()
 import           Data.Profunctor
 import qualified Data.Text                     as T
 
-type CalculatorOutput = Either CalcError [CalcResp]
+type CalculatorOutput = Either CalcError CalcResp
 type CalculatorBot = Bot IO CalcState Program CalculatorOutput
 
 calculatorBot :: CalculatorBot
-calculatorBot = Bot $ \program state ->
-  fmap (uncurry BotAction) $ interpretProgram program state
+calculatorBot = Bot $ \program state -> ListT $ do
+  o <- interpretProgram program state
+  runListT $ fmap (uncurry BotAction) $ bitraverse (traverse toListT) pure o
 
-parseErrorBot :: Applicative m => Bot m s ParseError [T.Text]
+parseErrorBot :: Monad m => Bot m s ParseError T.Text
 parseErrorBot = pureStatelessBot $ \ParseError {..} ->
-  pure $ "Failed to parse msg: \""
+  "Failed to parse msg: \""
     <> parseInput
     <> "\". Error message was: \""
     <> parseError
     <> "\"."
 
 simplifyCalculatorBot
-  :: Applicative m
-  => Bot m s Program (Either CalcError [CalcResp])
-  -> Bot m s T.Text [T.Text]
+  :: Monad m
+  => Bot m s Program (Either CalcError CalcResp)
+  -> Bot m s T.Text T.Text
 simplifyCalculatorBot bot =
   dimap parseProgram indistinct $ parseErrorBot \/ rmap printCalcOutput bot
 
-printCalcOutput :: Either CalcError [CalcResp] -> [T.Text]
+printCalcOutput :: Either CalcError CalcResp -> T.Text
 printCalcOutput = \case
-  Left  err   -> pure $ T.pack $ show err
-  Right resps -> resps <&> \case
-    Log e n -> T.pack $ show e <> " = " <> show n
+  Left  err   -> T.pack $ show err
+  Right resps -> resps & \case
+    Log e n   -> T.pack $ show e <> " = " <> show n
