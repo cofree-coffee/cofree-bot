1a4498858505c2264da44703c8bff8bf38c53d61
solomon <ssbothwell@gmail.com>
2022-11-20 19:10:06 -0800

    Updates matrix-client to 0.1.4.2 and ghc to 924. Removes brittany.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "Cofree.Coffee Matrix Bot";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
 
     flake-utils = {
       url = github:numtide/flake-utils;
@@ -15,7 +15,7 @@
 
   outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
     let
-      ghcVersion = "8107";
+      ghcVersion = "924";
       compiler = "ghc${ghcVersion}";
       # default systems compatible with pre-commit-hooks.nix
       # https://github.com/cachix/pre-commit-hooks.nix/pull/122
@@ -40,22 +40,7 @@
         hsPkgs = evalPkgs.haskell.packages.${compiler}.override {
           overrides = hfinal: hprev: {
             cofree-bot = hfinal.callCabal2nix "cofree-bot" ./. { };
-
-            # command to reproduce:
-            # cabal2nix https://github.com/softwarefactory-project/matrix-client-haskell --subpath matrix-client --revision f8610d8956bd146105292bb75821ca078d01b5ff > .nix/deps/matrix-client.nix
-            matrix-client = hfinal.callPackage ./nix/deps/matrix-client.nix { };
           };
-        };
-
-        brittany-config = pkgs.writeTextFile {
-          name = "brittany-config";
-          text = builtins.readFile ./brittany.yaml;
-        };
-
-        scripts = import ./nix/scripts.nix {
-          inherit brittany-config;
-          s = pkgs.writeShellScriptBin;
-          brittany = pkgs.haskellPackages.brittany;
         };
       in
       rec {
@@ -65,14 +50,13 @@
         devShell = pkgs.mkShell {
           inherit (self.checks.${system}.pre-commit-check) shellHook;
           buildInputs = with pkgs; [
-            haskellPackages.brittany
-            (haskell-language-server.override { supportedGhcVersions = [ ghcVersion ]; })
-            cabal-install
-            ghc
-            ghcid
             cabal2nix
+            cabal-install
+            ghcid
+            haskell.compiler.${compiler}
+            haskell.packages.${compiler}.haskell-language-server
             zlib
-          ] ++ (builtins.attrValues scripts);
+          ];
         };
 
         packages = flake-utils.lib.flattenTree {
@@ -88,13 +72,6 @@
             src = ./.;
             hooks = {
               nixpkgs-fmt.enable = true;
-              brittany = {
-                name = "brittany";
-                entry = "${hsPkgs.brittany}/bin/brittany --write-mode=inplace --config-file=${brittany-config}";
-                files = "\\.l?hs$";
-                language = "system";
-                pass_filenames = false;
-              };
               cabal-fmt.enable = true;
             };
           };
--- nix/scripts.nix
+++
@@ -1,4 +0,0 @@
-{ s, brittany, brittany-config }:
-{
-  format = s "format" "${brittany}/bin/brittany --write-mode=inplace --config-file=${brittany-config} src/**/*.hs";
-}
