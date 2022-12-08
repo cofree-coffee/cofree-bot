0851eeeb82180e5945da99415c8bbf319b771948
solomon <ssbothwell@gmail.com>
2022-12-08 12:48:47 -0800

    Bump nixpkgs to 22.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "Cofree.Coffee Matrix Bot";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
 
     flake-utils = {
       url = github:numtide/flake-utils;
