bbbe7184dd30428dd719a7f92479fc03d224d121
Solomon <ssbothwell@gmail.com>
2025-03-10 01:14:58 -0700

    Updates matrix-client version and adds run scripts. (#100)

--- flake.nix
+++ flake.nix
@@ -31,6 +31,7 @@
           compiler:
           evalPkgs.haskell.packages.${compiler}.override {
             overrides = hfinal: hprev: {
+              base64 = hfinal.base64_1_0;
               chat-bots = hfinal.callCabal2nix "chat-bots" ./chat-bots/. { };
               chat-bots-contrib = hfinal.callCabal2nix "chat-bots" ./chat-bots-contrib/. { };
               cofree-bot = hfinal.callCabal2nix "cofree-bot" ./cofree-bot/. { };
@@ -41,6 +42,12 @@
                 repo = "monoidal-functors";
                 rev = "e951a2be496d57d7f4e5b582ad175e8e97a9ab7b";
                 sha256 = "sha256-HfIMuU9yBp0JtN/ONOFku1wItbGLJl09fhaFzyiNVMg=";
+              }) { };
+              matrix-client = hfinal.callCabal2nix "matrix-client" (pkgs.fetchFromGitHub {
+                owner = "softwarefactory-project";
+                repo = "matrix-client-haskell";
+                rev = "57f13c2579fbcd9088fbb6b25046460e6cdbfdb0";
+                sha256 = "sha256-GNCnlaxWhg5pDi8JYlXyrqVIljr7NcNy+tWYSb+YYAg=";
               }) { };
             };
           };
@@ -98,7 +105,7 @@
           ghc982 = mkShellFor { compiler = "ghc982"; };
         };
 
-        packages = flake-utils.lib.flattenTree {
+        packages = flake-utils.lib.flattenTree rec {
           docker = import ./nix/docker.nix {
             inherit pkgs;
             cofree-bot = hsPkgs.cofree-bot;
@@ -110,7 +117,24 @@
           chat-bots = hsPkgs.chat-bots;
           chat-bots-contrib = hsPkgs.chat-bots-contrib;
           cofree-bot = hsPkgs.cofree-bot;
+          scripts = {
+            recurseForDerivations = true;
+            run-cofree-bot = pkgs.writeShellScriptBin "run-bot" ''
+              echo $CR_PAT | ${pkgs.docker}/bin/docker login ghcr.io -u solomon-b --password-stdin
+                ${pkgs.docker}/bin/docker compose -f docker-compose.yaml up --detach
+            '';
+            run-cofree-bot-cli = pkgs.writeShellScriptBin "run-bot" ''
+              ${cofree-bot}/bin/cofree-bot -- cli
+            '';
+          };
           default = hsPkgs.cofree-bot;
+        };
+
+        apps = {
+          run-cofree-bot = flake-utils.lib.mkApp { drv = self.packages.${system}."scripts/run-cofree-bot"; };
+          run-cofree-bot-cli = flake-utils.lib.mkApp {
+            drv = self.packages.${system}."scripts/run-cofree-bot-cli";
+          };
         };
 
         formatter = pkgs.nixfmt-rfc-style;
